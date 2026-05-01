bc11522252c543811b2f3c262d9b6f5a970ace22
Rahul-kr-code <mrrahul19042003@gmail.com>
2026-05-02 00:51:58 +0530

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -1,1 +1,32 @@
-bgjhgckugtlk/jnlmbikh
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+});
+
+// simple memory storage
+let content = "";
+
+io.on("connection", (socket) => {
+    console.log("User connected");
+
+    socket.emit("load", content);
+
+    socket.on("edit", (data) => {
+        content = data;
+        io.emit("update", data);
+    });
+});
+
+server.listen(4000, () => {
+    console.log("Server running on port 4000");
+});
